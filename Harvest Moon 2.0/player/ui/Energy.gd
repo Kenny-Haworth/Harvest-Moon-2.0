f6d10abad5c23a2a21ade30eafdbd90a1b858ee4
jeffreyn1 <sxephx@gmail.com>
2019-04-30 16:48:38 -0700

    Add files via upload

--- Harvest Moon 2.0/player/ui/Energy.gd
+++ Harvest Moon 2.0/player/ui/Energy.gd
@@ -1,12 +1,17 @@
 extends HBoxContainer
 
-var current = 25
+var current = 50
 var maximum = 50
 
 func _ready():
 	pass
-
+	
 func _process(delta):
 	$Bar.value = current
 	$Bar.max_value = maximum
 	$Number.set_text(str(current)+"/"+str(maximum))
+	
+func use_energy():
+	current = current - 1
+	$Bar.value = current
+	print(current)
